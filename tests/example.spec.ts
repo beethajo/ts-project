dd8366c762d180033a18094fe86512b1243d4e33
Anantha Chary Beethajo <anantha.beethajo@prolifics.com>
2025-07-09 08:42:52 +0530

    testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
+test.use({
+  baseURL: 'https://example.com' // ✅ Must include protocol
+});
+
+
+
+test.only('has title', async ({ page }) => {
+  await page.goto('');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -18,12 +25,4 @@ test('get started link', async ({ page }) => {
 });
 
 
-test('ci cd test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
