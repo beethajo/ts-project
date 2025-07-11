e42c4a3f656d56dcc0702bb436fe6feb4daf8fac
Anantha Chary Beethajo <anantha.beethajo@prolifics.com>
2025-07-11 10:56:31 +0530

    test cic cd today

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test('Login: Successful login with valid credentials', async ({ page }) => {
+test('Login 1: Successful login with valid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+
+  await loginPage.login('john_doe123', 'Password123');
+  await loginPage.verifyLoginSuccess();
+});
+
+
+test('Login 2 : Successful login with valid credentials', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
 
